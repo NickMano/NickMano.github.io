bd6217852d1cfad44ad37c8e9e0197de0348e4a2
Nico <nmanograsso@gmail.com>
2020-02-25 00:24:44 -0300

    Adjust design in cards

--- projects.js
+++ projects.js
@@ -2,42 +2,42 @@ const data = {
     "projets": [
         {
             "title": "Team Manager",
-            "tech": "En Wolox",
+            "tech": "meses",
             "description": "Lidero proyectos y gestiono el trabajo dentro del equipo del proyecto, orientándolo hacia la satisfacción del cliente externo y la mejora continua del servicio ofrecido.",
             "imageUrl": "images/wolox.jpeg",
             "imageAlt": "Wolox" 
         },
         {
-            "title": "The Legend of Nickest (En desarrollo)",
-            "tech": "Unity 2D / RPG",
-            "repo": "https://github.com/NickMano/The-Legend-Of-Nickest",
-            "description": "Debemos ayudar a Nickest a devolver la paz de su mundo encontrando distintos artefactos misticos que se encuentran dispersados en los templos sagrados.",
-            "imageUrl": "images/TLON.png",
-            "imageAlt": "The Legend of Nickest" 
+            "title": "JIRA to Sheet",
+            "tech": "Python 3",
+            "repo": "https://github.com/NickMano/JIRA-to-Sheet",
+            "description": "Creacion y automatizacion de Metricas en Poyectos que utilizan Jira Software.",
+            "imageUrl": "images/python.jpg",
+            "imageAlt": "Python" 
         },
         {
             "title": "Hangman Pokemon",
             "tech": "Python 3",
             "repo": "https://github.com/NickMano/Hangman-Pokemon",
-            "description": "El juego del Ahorcado con los 151 pokemons de la primera generacion. Debemos descubrir el nombre del pokemon oculto que es elegido de manera aleatoria a travez de una API.",
+            "description": "El juego del Ahorcado con los 151 pokemons de la primera generacion.",
             "imageUrl": "images/hangmanpkm.png",
             "imageAlt": "Pokeball" 
+        },
+        {
+            "title": "The Legend of Nickest (En desarrollo)",
+            "tech": "Unity 2D / RPG",
+            "repo": "https://github.com/NickMano/The-Legend-Of-Nickest",
+            "description": "Juego de plataforma, RPG y aventura. Inspirados en los juegos de Mario, Zelda y Metroid",
+            "imageUrl": "images/TLON.png",
+            "imageAlt": "The Legend of Nickest" 
         },
         {
             "title": "Nickest Jump",
             "tech": "Unity 2D / Mobile",
             "repo": "https://github.com/NickMano/Nickest-Jump",
-            "description": "Inspirado en el Dinausaurio de Chrome. <br/> Probando interaccion con el usuario, guardado de puntos localmente y generacion de enemigos.",
+            "description": "Inspirado en el Dinausaurio de Chrome.",
             "imageUrl": "images/NickestJump.png",
             "imageAlt": "Nickest Jump Hero" 
-        },
-        {
-            "title": "JIRA to Sheet",
-            "tech": "Python 3",
-            "repo": "https://github.com/NickMano/JIRA-to-Sheet",
-            "description": "Creacion y automatizacion de Metricas en Poyectos que utilizan Jira Software.<br/>Usando como soprte Google Sheet podemos ver el estado de las funcionalidades, los cambios de estado que ocurrió, cuanto tiempo estuvo en cada uno y cuantos retrabajos realizo el equipo",
-            "imageUrl": "images/python.jpg",
-            "imageAlt": "Python" 
         },
         {
             "title": "Simon Says",
@@ -46,14 +46,6 @@ const data = {
             "description": "El famoso juego de la seguir la secuencia de colores en el navegador hecho con JavaScript.",
             "imageUrl": "images/simon.png",
             "imageAlt": "Simon Says Logo" 
-        },
-        {
-            "title": "Este sitio",
-            "tech": "HTML / CSS / JS",
-            "repo": "https://github.com/Nick-m96/Nick-m96.github.io",
-            "description": "Proyecto hecho desde cero para practicar y mostrar los proyectos mas relevantes.",
-            "imageUrl": "images/photo.png",
-            "imageAlt": "Este Sitio" 
         }
     ]
 }
@@ -82,6 +74,10 @@ for (let i= 0; i < max; i++) {
     let figure = setSection('figure', 'portfoil-card__imageContainer')
     let img = setImage('portfoil-card__imageContainer--image', item.imageUrl, item.imageAlt)
     
+    if(item.title == 'Team Manager') {
+        tech = setSection('h5', 'project-couser', `${getMonthDiff()} ${item.tech}`)
+    }
+
     section.appendChild(article)
 
     article.appendChild(figure)
@@ -90,8 +86,8 @@ for (let i= 0; i < max; i++) {
     article.appendChild(divDetails)
     divDetails.appendChild(title)
     divDetails.appendChild(tech)
-    if (item.tech != 'En Wolox') {
-        let repo = setSection('p', 'project-url', `<strong>Puedes verlo en: </strong><a target="_blank" href="${item.repo}">GitHub</a>`)
+    if (item.repo) {
+        let repo = setSection('p', 'project-url', `<strong>Puedes verlo en: </strong><a target="_blank" class="link link--black" href="${item.repo}">GitHub</a>`)
         divDetails.appendChild(repo)
     }
     divDetails.appendChild(desc)
