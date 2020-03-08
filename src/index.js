709a29549c5cd090581dd84cb61b1d5fc0043057
Nico <nmanograsso@gmail.com>
2020-03-08 18:10:18 -0300

    Change path of images

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ const cards = [
         "title": "Team Manager",
         "badge": "meses",
         "description": "Lidero proyectos y gestiono el trabajo dentro del equipo del proyecto, orientándolo hacia la satisfacción del cliente externo y la mejora continua del servicio ofrecido.",
-        "imageUrl": "images/wolox.jpeg",
+        "imageUrl": "../images/wolox.jpeg",
         "imageAlt": "Wolox" 
     },
     {
@@ -11,7 +11,7 @@ const cards = [
         "badge": "Python 3",
         "repo": "https://github.com/NickMano/JIRA-to-Sheet",
         "description": "Creacion y automatizacion de Metricas en Poyectos que utilizan Jira Software.",
-        "imageUrl": "images/python.jpg",
+        "imageUrl": "../images/python.jpg",
         "imageAlt": "JIRA to Sheet" 
     },
     {
@@ -19,7 +19,7 @@ const cards = [
         "badge": "Web",
         "repo": "https://github.com/NickMano/platziVideo",
         "description": "Plataforma para mirar peliculas.",
-        "imageUrl": "images/platziVideo.png",
+        "imageUrl": "../images/platziVideo.png",
         "imageAlt": "platziVideo" 
     },
     {
@@ -27,7 +27,7 @@ const cards = [
         "badge": "Python 3",
         "repo": "https://github.com/NickMano/Hangman-Pokemon",
         "description": "El juego del Ahorcado con los 151 pokemons de la primera generacion.",
-        "imageUrl": "images/hangmanpkm.png",
+        "imageUrl": "../images/hangmanpkm.png",
         "imageAlt": "Pokeball" 
     },
     {
@@ -35,7 +35,7 @@ const cards = [
         "badge": "Unity 2D / RPG",
         "repo": "https://github.com/NickMano/The-Legend-Of-Nickest",
         "description": "Juego de plataforma, RPG y aventura. Inspirados en los juegos de Mario, Zelda y Metroid.",
-        "imageUrl": "images/TLON.png",
+        "imageUrl": "../images/TLON.png",
         "imageAlt": "The Legend of Nickest" 
     },
     {
@@ -43,7 +43,7 @@ const cards = [
         "badge": "Unity 2D / Mobile",
         "repo": "https://github.com/NickMano/Nickest-Jump",
         "description": "Juego inspirado en el Dinosaurio de Chrome.",
-        "imageUrl": "images/NickestJump.png",
+        "imageUrl": "../images/NickestJump.png",
         "imageAlt": "Nickest Jump Hero" 
     },
     {
@@ -51,7 +51,7 @@ const cards = [
         "badge": "Javascript",
         "repo": "https://github.com/NickMano/Simon",
         "description": "El famoso juego de la seguir la secuencia de colores en el navegador hecho con JavaScript.",
-        "imageUrl": "images/simon.png",
+        "imageUrl": "../images/simon.png",
         "imageAlt": "Simon Says Logo" 
     },
     {
@@ -59,7 +59,7 @@ const cards = [
         "badge": "iOS",
         "repo": "https://github.com/NickMano/wBooks-ios",
         "description": "wBooks es una aplicación que gestiona reservas de libros.",
-        "imageUrl": "images/wbooks.png",
+        "imageUrl": "../images/wbooks.png",
         "imageAlt": "wBooks" 
     }
 ]
