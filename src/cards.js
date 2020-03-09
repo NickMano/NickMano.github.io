1b6651bc367814c39c8a4719efd08dce31ca7370
Nico <nmanograsso@gmail.com>
2020-03-08 22:30:51 -0300

    Split js file and add modules

---
+++ src/cards.js
@@ -0,0 +1,67 @@
+const cards = [
+    {
+        "title": "Team Manager",
+        "badge": "meses",
+        "description": "Lidero proyectos y gestiono el trabajo dentro del equipo del proyecto, orientándolo hacia la satisfacción del cliente externo y la mejora continua del servicio ofrecido.",
+        "imageUrl": "../images/wolox.jpeg",
+        "imageAlt": "Wolox" 
+    },
+    {
+        "title": "JIRA to Sheet",
+        "badge": "Python 3",
+        "repo": "https://github.com/NickMano/JIRA-to-Sheet",
+        "description": "Creacion y automatizacion de Metricas en Poyectos que utilizan Jira Software.",
+        "imageUrl": "../images/python.jpg",
+        "imageAlt": "JIRA to Sheet" 
+    },
+    {
+        "title": "PlatziVideo (En desarrollo)",
+        "badge": "Web",
+        "repo": "https://github.com/NickMano/platziVideo",
+        "description": "Plataforma para mirar peliculas.",
+        "imageUrl": "../images/platziVideo.png",
+        "imageAlt": "platziVideo" 
+    },
+    {
+        "title": "Hangman Pokemon",
+        "badge": "Python 3",
+        "repo": "https://github.com/NickMano/Hangman-Pokemon",
+        "description": "El juego del Ahorcado con los 151 pokemons de la primera generacion.",
+        "imageUrl": "../images/hangmanpkm.png",
+        "imageAlt": "Pokeball" 
+    },
+    {
+        "title": "The Legend of Nickest (En desarrollo)",
+        "badge": "Unity 2D / RPG",
+        "repo": "https://github.com/NickMano/The-Legend-Of-Nickest",
+        "description": "Juego de plataforma, RPG y aventura. Inspirados en los juegos de Mario, Zelda y Metroid.",
+        "imageUrl": "../images/TLON.png",
+        "imageAlt": "The Legend of Nickest" 
+    },
+    {
+        "title": "Nickest Jump",
+        "badge": "Unity 2D / Mobile",
+        "repo": "https://github.com/NickMano/Nickest-Jump",
+        "description": "Juego inspirado en el Dinosaurio de Chrome.",
+        "imageUrl": "../images/NickestJump.png",
+        "imageAlt": "Nickest Jump Hero" 
+    },
+    {
+        "title": "Simon Says",
+        "badge": "Javascript",
+        "repo": "https://github.com/NickMano/Simon",
+        "description": "El famoso juego de la seguir la secuencia de colores en el navegador hecho con JavaScript.",
+        "imageUrl": "../images/simon.png",
+        "imageAlt": "Simon Says Logo" 
+    },
+    {
+        "title": "wBooks",
+        "badge": "iOS",
+        "repo": "https://github.com/NickMano/wBooks-ios",
+        "description": "wBooks es una aplicación que gestiona reservas de libros.",
+        "imageUrl": "../images/wbooks.png",
+        "imageAlt": "wBooks" 
+    }
+]
+
+export {cards}
