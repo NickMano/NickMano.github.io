d74ed31e149f45a62f63aa4376104b3be5a20280
Nico <nmanograsso@gmail.com>
2020-04-02 00:45:48 -0300

    Add new types of text

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -4,8 +4,8 @@ import '../assets/styles/components/Hero.scss'
 const Hero = () => (
     <section id="hero" className="hero">
       <div className="hero-container">
-        <h1 className="hero__name">Nicolas Manograsso</h1>
-        <h1 className="hero__title">Team Manager & Developer</h1>
+        <h1 className="text--title">Nicolas Manograsso</h1>
+        <h1 className="text--subtitle">Team Manager & Developer</h1>
       </div>
     </section>
 )
