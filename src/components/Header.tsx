7302776c9e77fc28ff622ac73372160f691d20a0
Nico <nmanograsso@gmail.com>
2020-04-02 00:03:31 -0300

    Change design of hero

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -5,16 +5,16 @@ const Header = (hasHero) => (
     <header className="header">
       <figure className="logo">
         {(hasHero == true) &&
-          <a href="#hero" className="link link--header">Nicolas Manograsso</a>
+          <a href="#hero" className="link--header">Nicolas Manograsso</a>
         }
       </figure>
       <nav className="menu">
         <ol className="header__list">
           <li>
-            <a className="link link--header" href="#portfoil">Portafolio</a>
+            <a className="link--header" href="#portfoil">Portafolio</a>
           </li>
           <li>
-            <a className="link link--header" href="https://github.com/NickMano/">Github</a>
+            <a className="link--header" href="https://github.com/NickMano/">Github</a>
           </li>
         </ol>
       </nav>
--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -3,9 +3,9 @@ import '../assets/styles/components/Hero.scss'
 
 const Hero = () => (
     <section id="hero" className="hero">
-      <div className="hero--fade-effect">
+      <div className="hero-container">
         <h1 className="hero__name">Nicolas Manograsso</h1>
-        <h1 className="hero__title">Team Manager con pasion por el Desarrollo</h1>
+        <h1 className="hero__title">Team Manager & Developer</h1>
       </div>
     </section>
 )
