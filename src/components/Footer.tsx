0cdc001d7a6da9ac361caa583b41cfa1495ab3a3
Nico <nmanograsso@gmail.com>
2020-04-09 23:17:11 -0300

    Update footer style

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,16 +1,14 @@
 import React from "react";
 import '../assets/styles/components/Footer.scss'
-import '../assets/styles/components/Contact.scss'
-import '../assets/styles/components/Input.scss'
 
 const Footer = () => (
-    <footer id="contact" className="footer">
-      <form action="/suscripcion/" className="contact">
-          <h3>¿Creamos algo juntos?</h3>
-          <input type="text" placeholder="Déjame tu mail" className="input" />
-          <textarea placeholder="Motivo" className="input input-textarea"></textarea>
-          <button className="button button--red" disabled>Enviar</button>
-        </form> 
+    <footer className="footer">
+        <div className="footer__container">
+          <h2>
+            Interested in working together? <br />
+            Contact me at nmanograsso@gmail.com 
+          </h2>
+        </div>
     </footer>
 )
 
