4af99679c263b76eec67bcd994470995dff3647f
Nico <nmanograsso@gmail.com>
2020-04-11 18:23:13 -0300

    Remove typescript

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,8 +1,9 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 import '../assets/styles/components/Card.scss'
 import '../assets/styles/components/Badge.scss'
 
-const Card = ({link, imageUrl}) => {
+const Card = ({title, link, imageUrl}) => {
     const getMonthDiff = () => {
         const TM = new Date(2019, 9, 3)
         const today = new Date()
@@ -13,9 +14,7 @@ const Card = ({link, imageUrl}) => {
     }
 
     return(
-        <a className="card" href={link} 
-        style={{backgroundImage: `url(${imageUrl})`}}
-        />
+            <Link to={`/${title}`} className="card" style={{backgroundImage: `url(${imageUrl})`}} />
     )
 }
 
--- src/components/Portfoil.jsx
+++ src/components/Portfoil.jsx
@@ -1,12 +1,7 @@
 import React from 'react';
 import '../assets/styles/components/Portfoil.scss';
 
-interface PortfoilProps {
-    title: string,
-    children: any
-}
-
-const Portfoil = ({children}:PortfoilProps) => (
+const Portfoil = ({children}) => (
     <section id="portfoil" className="portfoil">
         {children}
     </section>
