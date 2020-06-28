3599bd00918818d901fabda5a3d77b722ff701ca
Nico <nmanograsso@gmail.com>
2020-06-28 14:41:46 -0300

    Remove unnecessary references

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,9 +1,8 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
 import '../assets/styles/components/Card.scss'
-import '../assets/styles/components/Badge.scss'
 
-const Card = ({title, link, imageUrl, category}) => (
+const Card = ({title, imageUrl, category}) => (
     <Link to={`/${category}/${title}`} className="card" style={{backgroundImage: `url(${imageUrl})`}} />
 )
 
