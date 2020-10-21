1ddedab4bba38a1d9921e4cdc6f108a4d90819d4
Nico <nmanograsso@gmail.com>
2020-10-21 19:51:13 -0300

    Add new project on website

--- src/index.js
+++ src/index.js
@@ -6,40 +6,54 @@ import reducer from './reducers'
 import App from './routes/App'
 
 const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose
+const imageRoute = "/src/images/"
 const initialState = {
     "websites": [
+        {
+            "title": "Rick and Morty - Interdimesional Conference",
+            "description": "This project is a training of react using hooks and react router",
+            "link": "https://github.com/NickMano/training-react--rick-and-morty-interdimensional-conf",
+            "imageUrl": `${imageRoute}rick-and-morty-conf/cover.png`,
+            "hero": `${imageRoute}rick-and-morty-conf/hero.png`,
+            "initialImage": `${imageRoute}rick-and-morty-conf/initialImage.png`,
+            "images": [
+                `${imageRoute}rick-and-morty-conf/image-1.png`,
+                `${imageRoute}rick-and-morty-conf/image-2.png`,
+                `${imageRoute}rick-and-morty-conf/image-3.png`,
+            ]
+        },
         {
             "title": "Pokedex",
             "description": "This is a modern and minimalist pokedex",
             "link": "https://nicomano-pokedex.netlify.app/",
-            "imageUrl": "/src/assets/static/pokedex/pokedex.webp",
-            "hero": "/src/assets/static/pokedex/hero.webp",
-            "initialImage": '/src/assets/static/pokedex/initialImage.webp',
+            "imageUrl": `${imageRoute}pokedex/pokedex.webp`,
+            "hero": `${imageRoute}pokedex/hero.webp`,
+            "initialImage": `${imageRoute}pokedex/initialImage.webp`,
             "images": [
-                '/src/assets/static/pokedex/image-3.webp',
-                '/src/assets/static/pokedex/image-1.webp',
-                '/src/assets/static/pokedex/image-4.webp',
-                '/src/assets/static/pokedex/image-2.webp',
-                '/src/assets/static/pokedex/image-6.webp',
+                `${imageRoute}pokedex/image-3.webp`,
+                `${imageRoute}pokedex/image-1.webp`,
+                `${imageRoute}pokedex/image-4.webp`,
+                `${imageRoute}pokedex/image-2.webp`,
+                `${imageRoute}pokedex/image-6.webp`,
             ]
         },
         {
             "title": "PlatziVideo",
             "link": "https://github.com/NickMano/platziVideo",
-            "imageUrl": "/src/assets/static/platziVideo.webp",
+            "imageUrl": `${imageRoute}platziVideo.webp`,
         },
         {
             "title": "Simon Says",
             "description": "Memorize the sequence to win",
             "link": "https://nicomano-simon.netlify.app/",
-            "imageUrl": "/src/assets/static/simon/simon.webp",
-            "hero": "/src/assets/static/simon/hero-1.webp",
-            "initialImage": '/src/assets/static/simon/initialImage.webp',
+            "imageUrl": `${imageRoute}simon/simon.webp`,
+            "hero": `${imageRoute}simon/hero-1.webp`,
+            "initialImage": `${imageRoute}simon/initialImage.webp`,
             "images": [
-                '/src/assets/static/simon/image-1.webp',
-                '/src/assets/static/simon/image-2.webp',
-                '/src/assets/static/simon/image-3.webp',
-                '/src/assets/static/simon/image-4.webp',
+                `${imageRoute}simon/image-1.webp`,
+                `${imageRoute}simon/image-2.webp`,
+                `${imageRoute}simon/image-3.webp`,
+                `${imageRoute}simon/image-4.webp`,
             ]
         }
     ],
@@ -47,50 +61,50 @@ const initialState = {
         {
             "title": "The-Legend-Of-Nickest",
             "link": "https://github.com/NickMano/The-Legend-Of-Nickest",
-            "imageUrl": "/src/assets/static/tlon/TLON.webp",
+            "imageUrl": `${imageRoute}tlon/TLON.webp`,
             "description": "Game of platform and action, inspired by Mario and Zelda",
-            "hero": "/src/assets/static/tlon/hero.webp",
-            "initialImage": '/src/assets/static/tlon/initialImage.webp',
+            "hero": `${imageRoute}tlon/hero.webp`,
+            "initialImage": `${imageRoute}tlon/initialImage.webp`,
             "images": [
-                '/src/assets/static/tlon/image-1.webp',
-                '/src/assets/static/tlon/image-2.webp',
-                '/src/assets/static/tlon/image-4.webp',
+                `${imageRoute}tlon/image-1.webp`,
+                `${imageRoute}tlon/image-2.webp`,
+                `${imageRoute}tlon/image-4.webp`,
             ]
         },
         {
             "title": "Nickest-Jump",
             "description": "Infinite runner like Chrome Dinosaur",
             "link": "https://nicomano.itch.io/nickest-jump",
-            "imageUrl": "/src/assets/static/nickest-jump/NickestJump.webp",
-            "hero": "/src/assets/static/nickest-jump/hero.webp",
-            "initialImage": '/src/assets/static/nickest-jump/initialImage.webp',
+            "imageUrl": `${imageRoute}nickest-jump/NickestJump.webp`,
+            "hero": `${imageRoute}nickest-jump/hero.webp`,
+            "initialImage": `${imageRoute}nickest-jump/initialImage.webp`,
             "images": [
-                '/src/assets/static/nickest-jump/image-4.webp',
-                '/src/assets/static/nickest-jump/image-2.webp',
-                '/src/assets/static/nickest-jump/image-3.webp',
-                '/src/assets/static/nickest-jump/image-1.webp',
+                `${imageRoute}nickest-jump/image-4.webp`,
+                `${imageRoute}nickest-jump/image-2.webp`,
+                `${imageRoute}nickest-jump/image-3.webp`,
+                `${imageRoute}nickest-jump/image-1.webp`,
             ]
         },
         {
             "title": "Ping-Pong",
             "description": "The classic game of Ping-Pong now on web",
             "link": "https://nicomano.itch.io/ping-pong",
-            "imageUrl": "/src/assets/static/ping-pong/ping-pong.webp",
-            "hero": "/src/assets/static/ping-pong/hero.webp",
-            "initialImage": '/src/assets/static/ping-pong/initialImage.webp',
+            "imageUrl": `${imageRoute}ping-pong/ping-pong.webp`,
+            "hero": `${imageRoute}ping-pong/hero.webp`,
+            "initialImage": `${imageRoute}ping-pong/initialImage.webp`,
             "images": [
-                '/src/assets/static/ping-pong/image-3.webp',
-                '/src/assets/static/ping-pong/image-1.webp',
-                '/src/assets/static/ping-pong/image-2.webp',
+                `${imageRoute}ping-pong/image-3.webp`,
+                `${imageRoute}ping-pong/image-1.webp`,
+                `${imageRoute}ping-pong/image-2.webp`,
             ]
         },
         {
             "title": "Hangman-Pokemon",
             "link": "https://github.com/NickMano/Hangman-Pokemon",
             "description": "We must guess the pokemon game. Only the 1 generation",
-            "imageUrl": "/src/assets/static/hangmanpkm/hangmanpkm.webp",
-            "hero": "/src/assets/static/hangmanpkm/hero.webp",
-            "initialImage": '/src/assets/static/hangmanpkm/initialImage.webp',
+            "imageUrl": `${imageRoute}hangmanpkm/hangmanpkm.webp`,
+            "hero": `${imageRoute}hangmanpkm/hero.webp`,
+            "initialImage": `${imageRoute}hangmanpkm/initialImage.webp`,
             "images": []
         },
     ],
@@ -98,18 +112,18 @@ const initialState = {
         {
             "title": "wBooks-iOS",
             "link": "https://github.com/NickMano/wBooks-ios",
-            "imageUrl": "/src/assets/static/wbooks/wbooks.webp",
+            "imageUrl": `${imageRoute}wbooks/wbooks.webp`,
             "description": "wBooks is a test application that manages book reservations.",
-            "hero": "/src/assets/static/wbooks/hero.webp",
-            "initialImage": '/src/assets/static/wbooks/initialImage.webp',
+            "hero": `${imageRoute}wbooks/hero.webp`,
+            "initialImage": `${imageRoute}wbooks/initialImage.webp`,
             "images": [
-                '/src/assets/static/wbooks/image-1.webp',
+                `${imageRoute}wbooks/image-1.webp`,
             ]
         },
         {
             "title": "Jira-To-Sheet",
             "link": "https://github.com/NickMano/JIRA-to-Sheet",
-            "imageUrl": "/src/assets/static/python.webp", 
+            "imageUrl": `${imageRoute}python.webp`, 
         },
     ]
 }
