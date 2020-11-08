9f9e0b2c1a0efbb8208bad43a468d5ceb7be8472
Nico <nmanograsso@gmail.com>
2020-11-07 23:02:41 -0300

    Format principalImage

--- src/components/PrincipalImage.jsx
+++ src/components/PrincipalImage.jsx
@@ -1,11 +1,10 @@
-import React from 'react'
-import '../styles/components/PrincipalImage.scss'
+import React from 'react';
+import '../styles/components/PrincipalImage.scss';
 
-const PrincipalImage = ({url}) => (
-    <>
-        <img className='principal-image' src={url} />
-    </>
-)
+const PrincipalImage = ({ url }) => (
+  <>
+    <img className="principal-image" src={url} />
+  </>
+);
 
-
-export default PrincipalImage
+export default PrincipalImage;
