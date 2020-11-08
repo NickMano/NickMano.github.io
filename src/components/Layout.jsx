6dcc6f6597dfd484b91eaef520a5f790d0b2e0aa
Nico <nmanograsso@gmail.com>
2020-11-07 23:03:16 -0300

    Add prop types to layout

--- src/components/Layout.jsx
+++ src/components/Layout.jsx
@@ -1,13 +1,18 @@
-import React from 'react'
-import Footer from './Footer'
-import Header from './Header'
- 
-const Layout = ({children}) => (
-    <>
-        <Header />
-        {children}
-        <Footer />
-    </>
-)
+import React from 'react';
+import PropTypes from 'prop-types';
+import Footer from './Footer';
+import Header from './Header';
 
-export default Layout
+const Layout = ({ children }) => (
+  <>
+    <Header />
+    {children}
+    <Footer />
+  </>
+);
+
+Layout.propTypes = {
+  children: PropTypes.element.isRequired,
+};
+
+export default Layout;
