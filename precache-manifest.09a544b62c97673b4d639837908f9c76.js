1de6371ecd1b4707545d665d48afec0b67c1960b
Nico <nmanograsso@gmail.com>
2020-11-08 12:51:13 -0300

    Updates

---
+++ precache-manifest.09a544b62c97673b4d639837908f9c76.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f56a821c11b804dd36c264cbc5edb3f7",
+    "url": "/NickMano.github.io/index.html"
+  },
+  {
+    "revision": "76196ec9984082bda60c",
+    "url": "/NickMano.github.io/static/css/main.3d6c61bd.chunk.css"
+  },
+  {
+    "revision": "a660a490c7b83200b74a",
+    "url": "/NickMano.github.io/static/js/2.1cbba660.chunk.js"
+  },
+  {
+    "revision": "9b318b6fb13190fe82c0677e9264b3c7",
+    "url": "/NickMano.github.io/static/js/2.1cbba660.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "76196ec9984082bda60c",
+    "url": "/NickMano.github.io/static/js/main.0b90fbe1.chunk.js"
+  },
+  {
+    "revision": "eeeda840cd67b32aa0b7",
+    "url": "/NickMano.github.io/static/js/runtime-main.11b935a8.js"
+  }
+]);
