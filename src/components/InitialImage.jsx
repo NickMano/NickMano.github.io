207829f032ec3a1ee11a0db4d5fc763e598820d4
Nico <nmanograsso@gmail.com>
2020-11-07 23:02:13 -0300

    Add prop types to initialImage component

--- src/components/InitialImage.jsx
+++ src/components/InitialImage.jsx
@@ -1,14 +1,24 @@
-import React from 'react'
-import '../styles/components/InitialImage.scss'
+import React from 'react';
+import PropTypes from 'prop-types';
+import '../styles/components/InitialImage.scss';
 
-const InitialImage = props => {
-    const {url, isInitialImage} = props
-    const classImage = isInitialImage ? 'initial-image' : 'collection-image'
-    return(
-        <section className='initial-image-container'>
-            <img className={classImage} src={`${url}`} />
-        </section>
-    )
-}
+const InitialImage = (props) => {
+  const { url, isInitialImage } = props;
+  const classImage = isInitialImage ? 'initial-image' : 'collection-image';
+  return (
+    <section className="initial-image-container">
+      <img className={classImage} src={`${url}`} alt={classImage} />
+    </section>
+  );
+};
 
-export default InitialImage
+InitialImage.propTypes = {
+  url: PropTypes.string.isRequired,
+  isInitialImage: PropTypes.bool,
+};
+
+InitialImage.defaultProps = {
+  isInitialImage: false,
+};
+
+export default InitialImage;
