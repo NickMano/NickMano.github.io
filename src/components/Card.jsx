94a7084cdb5479f5bdd27cc098b0b615a71f7f5c
Nico <nmanograsso@gmail.com>
2020-11-07 22:59:04 -0300

    Add prop types to card component

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,9 +1,16 @@
-import React from 'react'
-import { Link } from 'react-router-dom'
-import '../styles/components/Card.scss'
+import React from 'react';
+import { Link } from 'react-router-dom';
+import PropTypes from 'prop-types';
+import '../styles/components/Card.scss';
 
-const Card = ({title, imageUrl, category}) => (
-    <Link to={`/${category}/${title}`} className="card" style={{backgroundImage: `url(${imageUrl})`}} />
-)
+const Card = ({ title, imageUrl, category }) => (
+  <Link to={`/${category}/${title}`} className="card" style={{ backgroundImage: `url(${imageUrl})` }} />
+);
 
-export default Card
+Card.propTypes = {
+  title: PropTypes.string.isRequired,
+  imageUrl: PropTypes.string.isRequired,
+  category: PropTypes.string.isRequired,
+};
+
+export default Card;
