d0285474a38a5e06b8d6b84e913826ed7176dfba
Nico <nmanograsso@gmail.com>
2020-11-07 23:01:00 -0300

    Add prop types to footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,14 +1,15 @@
-import React from "react";
-import '../styles/components/Footer.scss'
+import React from 'react';
+import '../styles/components/Footer.scss';
 
 const Footer = () => (
   <footer className="footer">
     <hr />
     <h2>
-      Interested in working together? <br />
-      Contact me at nmanograsso@gmail.com 
+      Interested in working together?
+      <br />
+      Contact me at nmanograsso@gmail.com
     </h2>
   </footer>
-)
+);
 
-export default Footer
+export default Footer;
