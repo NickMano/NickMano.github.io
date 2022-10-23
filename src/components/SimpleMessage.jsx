3345bf5ebc5dd3dc102ed6dcd54c819e8b865d85
Nico <nmanograsso@gmail.com>
2022-10-23 20:44:03 -0300

    add: support to dark mode on wip page

--- src/components/SimpleMessage.jsx
+++ src/components/SimpleMessage.jsx
@@ -1,10 +1,13 @@
 import React from 'react';
+import { useTheme } from '../hooks/useTheme';
 import '../styles/components/SimpleMessage.scss';
 
-const SimpleMessage = ({ message }) => (
-  <>
-    <h1 className="fullScreen">{message}</h1>
-  </>
-);
+const SimpleMessage = ({ message }) => {
+  const { mode } = useTheme()
+
+  return(
+      <h1 className={`fullScreen ${mode}`}>{message}</h1>
+  )
+};
 
 export default SimpleMessage;
